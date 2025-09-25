58dca9e57eb62964432246c3e4c1263a6efa9adf
Cameron-Love <ctlove@aggies.ncat.edu>
2025-09-24 20:59:29 -0400

    Pass Test Case 1: Initial game setup with required variables

--- ctlove_assignment_4.py
+++ ctlove_assignment_4.py
@@ -1,1 +1,7 @@
+student_name = "Cameron Love"
+current_gpa = 4.0
+study_hours = 21
+social_points = 60
+stress_level = 35
 
+print(f"Welcome! Here are your stats: Name: {student_name} Current GPA: {current_gpa} Study Hours: {study_hours} Social Points: {social_points} Stress Level: {stress_level}")
