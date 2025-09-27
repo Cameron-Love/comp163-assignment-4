56aaf89148b3a3228a39b8885dbb8ffef5a801bc
Cameron-Love <ctlove@aggies.ncat.edu>
2025-09-26 21:38:45 -0400

    Pass Test Case 4: Complete assessment with all concepts and multiple endings

--- ctlove_assignment_4.py
+++ ctlove_assignment_4.py
@@ -25,7 +25,7 @@ if choice == "A":
             study_hours = study_hours + 10
             print("You need to study more!")
         else:
-            print("You are studying a good amount. Maybe ask your professor for help.")
+            print("You are studying a good amount. Maybe ask your professor for help and your GPA may increase.")
         if social_points >= 60:
             print("Stop socializing and lock in!")
     elif current_gpa <= 1.0:
@@ -180,3 +180,98 @@ if study_choice in study_options:
                 print("Go make some friends! You have the time.")
 elif study_choice not in study_options:
     print("Invalid Choice. Try again.")
+
+print("")
+print("Time for your final exam!")
+favorite_number_answer = 7
+faorite_number = 0
+if social_points >= 50:
+    print("At the beginnging of the school year the teacher told you their favorite number and you remembered for an extra point!")
+    favorite_number = favorite_number_answer
+else:
+    print("At the beginnging of the school year the teacher told you their favorite number and asked the class before the exam for an extra point, but you forgot!")
+    favorite_number_answer = favorite_number_answer + 100000
+exam_score = 0
+if current_gpa >= 3.5:
+    if study_hours >= 20:
+        print("You studied and did amazing!")
+        exam_score = 95
+        print(f"Exam score: {exam_score}")
+        if not social_points >= 50 or stress_level <= 20:
+            print("You saw the professor before the final exam and you had a good conversation about golf! Afterward you both played golf together and you ended up getting 5 extra pointson your exam!")
+            exam_score = exam_score + 5
+            print(f"Exam score: {exam_score}")
+        elif favorite_number is favorite_number_answer:
+            print("Here is your extra point.")
+            exam_score = exam_score + 1
+        elif favorite_number is not favorite_number_answer:
+            print("You geussed the wrong number. No extra point for you")
+    elif not study_hours >= 20:
+        print("You are smart, but you didn't study enough to ace the exam!")
+        exam_score = 90
+        print(f"Exam score: {exam_score}")
+        if not social_points >= 50 or stress_level <= 20:
+            print("You saw the professor before the final exam and you had a good conversation about golf! Afterward you both played golf together and you ended up getting 5 extra pointson your exam!")
+            exam_score = exam_score + 5
+            print(f"Exam score: {exam_score}")
+        elif favorite_number is favorite_number_answer:
+            print("Here is your extra point.")
+            exam_score = exam_score + 1
+        elif favorite_number is not favorite_number_answer:
+            print("You geussed the wrong number. No extra point for you")
+elif current_gpa >= 3.0:
+    if study_hours >= 20:
+        print("You studied and a pretty good job!")
+        exam_score = 90
+        print(f"Exam score: {exam_score}")
+        if not social_points >= 50 or stress_level <= 20:
+            print("You saw the professor before the final exam and you had a good conversation about golf! Afterward you both played golf together and you ended up getting 5 extra pointson your exam!")
+            exam_score = exam_score + 5
+            print(f"Exam score: {exam_score}")
+        elif favorite_number is favorite_number_answer:
+            print("Here is your extra point.")
+            exam_score = exam_score + 1
+        elif favorite_number is not favorite_number_answer:
+            print("You geussed the wrong number. No extra point for you")
+    elif not study_hours >= 20:
+        print("You are quite smart, but you didn't study enough to get an A on the exam!")
+        exam_score = 80
+        print(f"Exam score: {exam_score}")
+        if not social_points >= 50 or stress_level <= 20:
+            print("You saw the professor before the final exam and you had a good conversation about golf! Afterward you both played golf together and you ended up getting 5 extra pointson your exam!")
+            exam_score = exam_score + 5
+            print(f"Exam score: {exam_score}")
+        elif favorite_number is favorite_number_answer:
+            print("Here is your extra point.")
+            exam_score = exam_score + 1
+        elif favorite_number is not favorite_number_answer:
+            print("You geussed the wrong number. No extra point for you")
+elif current_gpa >= 2.5:
+    if study_hours >= 20:
+        print("You studied and did your best!")
+        exam_score = 80
+        print(f"Exam score: {exam_score}")
+        if not social_points >= 50 or stress_level <= 20:
+            print("You saw the professor before the final exam and you had a good conversation about golf! Afterward you both played golf together and you ended up getting 5 extra pointson your exam!")
+            exam_score = exam_score + 5
+            print(f"Exam score: {exam_score}")
+        elif favorite_number is favorite_number_answer:
+            print("Here is your extra point.")
+            exam_score = exam_score + 1
+        elif favorite_number is not favorite_number_answer:
+            print("You geussed the wrong number. No extra point for you")
+    elif not study_hours >= 20:
+        print("You know you needed to study, but didn't. So you didn't do as good as you wanted, but still passed.")
+        exam_score = 70
+        print(f"Exam score: {exam_score}")
+        if not social_points >= 50 or stress_level <= 20:
+            print("You saw the professor before the final exam and you had a good conversation about golf! Afterward you both played golf together and you ended up getting 5 extra pointson your exam!")
+            exam_score = exam_score + 5
+            print(f"Exam score: {exam_score}")
+        elif favorite_number is favorite_number_answer:
+            print("Here is your extra point.")
+            exam_score = exam_score + 1
+        elif favorite_number is not favorite_number_answer:
+            print("You geussed the wrong number. No extra point for you")
+else:
+    print("YOU FAILED. TIME TO DROP OUT BUD")
